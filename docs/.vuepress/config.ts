6ac0621495cb2591eea1bf8389bf0db52ea5fa3e
PH <111343367+im-ph@users.noreply.github.com>
2025-04-20 13:30:42 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -16,7 +16,7 @@ export default defineUserConfig({
     logo: 'https://www.mefrp.com/favicon.ico',
     hostname: "https://api.mefrp.com",
     autoFrontmatter: false,
-    footer: { message: "", "copyright": "Copyright © ME Frp 项目组 2021-2025.<br><a target='_blank' href='https://beian.miit.gov.cn/'>蜀 ICP 备 2023020251 号</a><br><a href='https://内网穿透.中国/'>Frp 内网穿透联盟统一识别编码: AZWB66WB</a>" },
+    footer: { message: "", "copyright": "Copyright © ME Frp 项目组 2021-2025.<br><br><a href='https://内网穿透.中国/'>Frp 内网穿透联盟统一识别编码: AZWB66WB</a>" },
     navbar: [
       {
         text: '主页',
